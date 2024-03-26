c7d9f044dc951a8588194b30cdd957b20bcaf51a
Albro3459 <brodsky.alex22@gmail.com>
2024-03-26 16:53:00 -0500

    re added gitignore

--- Recipe_Proj.Server/obj/Debug/net8.0/Recipe_Proj.Server.AssemblyInfo.cs
+++ Recipe_Proj.Server/obj/Debug/net8.0/Recipe_Proj.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recipe_Proj.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ae8c02b20b9d9cbcd5c38b3dd2724b2bde5ba4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+940fa170c669776a0e0bc050bec1a97f79b7626c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recipe_Proj.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recipe_Proj.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
