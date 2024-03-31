e358794b675a26e834711d5b15842ae2bae745a3
Albro3459 <brodsky.alex22@gmail.com>
2024-03-31 00:07:53 -0500

    setupo configuring https and cross resource sharing

--- Recipe_Proj.Server/Program.cs
+++ Recipe_Proj.Server/Program.cs
@@ -1,22 +1,27 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Recipe_Proj.Server.Database;
 using Recipe_Proj.Server.Services;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddDbContext<RecipeDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
     ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
 
 builder.Services.AddScoped<IRecipeService, RecipeService>();
-
 builder.Services.AddControllers();
 
+// Configure CORS to allow requests from your Blazor client
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorClient", policyBuilder =>
+        policyBuilder.WithOrigins("https://localhost:7106") // Add your Blazor client's URL here
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -30,14 +35,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
+// Cross Resource Sharing for WebAssembly Client
+app.UseCors("AllowBlazorClient");
+
+// Uncomment the following line if your application uses authentication/authorization.
 // app.UseAuthorization();
 
 app.MapControllers(); // Maps controllers to API endpoints
-
 
 app.Run();
