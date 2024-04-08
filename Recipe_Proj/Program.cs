9abb00143a26efc5041a393ff1a55d83174bb58a
Albro3459 <brodsky.alex22@gmail.com>
2024-04-08 02:54:33 -0500

    made recipe search and added restrictions dropdown. Need to add searching with restrictions

--- Recipe_Proj/Program.cs
+++ Recipe_Proj/Program.cs
@@ -8,6 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<IRecipeService, RecipeService>();
+builder.Services.AddScoped<IRestrictionService, RestrictionService>();
+builder.Services.AddScoped<IIngredientService, IngredientService>();
 
 
 // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
---
+++ Recipe_Proj/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+// function initializeMultiSelectDropdown(selectorId) {
+//     console.log($('#' + selectorId));
+//     $('#' + selectorId).selectpicker();
+// }
+
+// window.initializeSelect2 = function()  {
+//     $('#restriction-multi-select').select2({
+//         theme: 'bootstrap-5',
+//         width: $('#restriction-multi-select').data('width') ? $('#restriction-multi-select').data('width') : $('#restriction-multi-select').hasClass('w-100') ? '100%' : 'style',
+//         placeholder: $('#restriction-multi-select').data('placeholder'),
+//         closeOnSelect: false,
+//         dropdownParent: $('#restriction-multi-select').parent()
+//     });
+// }
