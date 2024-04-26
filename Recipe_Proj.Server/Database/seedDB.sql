952493f743e36c365e766608fa2486b45aa437b3
Albro3459 <brodsky.alex22@gmail.com>
2024-04-25 23:07:27 -0500

    added bbq recipe

--- Recipe_Proj.Server/Database/seedDB.sql
+++ Recipe_Proj.Server/Database/seedDB.sql
@@ -6,61 +6,10 @@ INSERT INTO recipe (recipeID, recipeName, shortDescription, recipeInstructions, 
 (1005, 'Steak, Green Beans, and Potatoes', 'savor a classic dinner of juicy steak paired with crispy roasted potatoes and tender-crisp green beans, all seasoned to perfection', 'steak_greenbeans_potatoes.json', 'steak_greenbeans_potatoes', 40, 700, 35, 12, 0.5, 115, 600, 45, 6, 3, 45),
 (1006, 'Meat Lasagna', 'savor this classic Meat Lasagna, featuring layers of Italian sausage, ground beef, rich cheeses, and a flavorful tomato sauce, all baked to perfection for a comforting dinner delight.', 'meat_lasagna.json', 'meat_lasagna', 70, 810, 40, 18, 1, 153, 1440, 63, 5.5, 11, 57.5),
 (1007, 'Creamy Vegan Pasta with Broccoli', 'a delightful blend of al dente pasta and vibrant broccoli smothered in a lush, vegan white bean sauce, enhanced with a sprinkle of crunchy pine nuts', 'creamy_vegan_pasta_broccoli.json', 'creamy_vegan_pasta_broccoli', 30, 675, 18, 2.25, 0, 0, 225, 100.5, 18, 6, 27),
-(1008, 'Blueberry Pie', 'a classic dessert featuring a flaky crust filled with sweet and tart blueberries, subtly enhanced with lemon zest and cinnamon for a delightful flavor', 'blueberry_pie.json', 'blueberry_pie', 50, 330, 15, 9, 0, 80, 300, 45, 3, 22, 3);
+(1008, 'Blueberry Pie', 'a classic dessert featuring a flaky crust filled with sweet and tart blueberries, subtly enhanced with lemon zest and cinnamon for a delightful flavor', 'blueberry_pie.json', 'blueberry_pie', 50, 330, 15, 9, 0, 80, 300, 45, 3, 22, 3),
+(1009, 'BBQ Tri-Tip, Pulled Pork, Mac n Cheese, Roasted Vegetables', 'a hearty meal featuring succulent BBQ tri tip, tender pulled pork, creamy mac and cheese, and roasted broccoli, offering a satisfying balance of flavors and textures', 'bbq_tritip_pulledpork_macncheese_broccoli.json', 'bbq_tritip_pulledpork_macncheese_broccoli', 120, 970, 58, 22, 0, 225, 870, 52, 5, 8, 68);
 
 
-INSERT INTO restriction (restrictionID, restrictionName) VALUES 
-(1001, 'Low-carb'),
-(1002, 'Keto'),
-(1003, 'Vegan'),
-(1004, 'Vegetarian'),
-(1005, 'Pescatarian'),
-(1006, 'Low-fat'),
-(1007, 'Paleo'),
-(1008, 'Carnivore'),
-(1009, 'High-protein'), -- only with a (cal/10)/protein ratio <= 10/7 or 1.43
-(1010, 'Gluten-free'),
-(1011, 'Dairy-free'),
-(1012, 'Nut-free'),
-(1013, 'Egg-free'),
-(1014, 'Soy-free'),
-(1015, 'Grain-free'),
-(1016, 'Sugar-free'),
-(1017, 'Whole30'),
-(1018, 'Mediterranean'),
-(1019, 'Quick Meal'); -- 20 min or less
-
-INSERT INTO recipe_restriction (recipeID, restrictionID) VALUES 
-(1001, 1012), -- Nut-free
-(1002, 1006), -- Low-fat
-(1002, 1010), -- Gluten-free
-(1002, 1011), -- Dairy-free
-(1002, 1012), -- Nut-free
-(1003, 1010), -- Gluten-free
-(1003, 1011), -- Dairy-free
-(1003, 1012), -- Nut-free
-(1003, 1016), -- Sugar-free
-(1004, 1005), -- Pescatarian
-(1004, 1010), -- Gluten-free
-(1004, 1011), -- Dairy-free
-(1004, 1012), -- Nut-free
-(1004, 1013), -- Egg-free
-(1004, 1018), -- Mediterranean
-(1005, 1010), -- Gluten-free
-(1005, 1012), -- Nut-free
-(1005, 1013), -- Egg-free
-(1005, 1014), -- Soy-free
-(1005, 1015), -- Grain-free
-(1006, 1012), -- Nut-free
-(1006, 1014), -- Soy-free
-(1007, 1003), -- Vegan
-(1007, 1004), -- Vegetarian
-(1007, 1011), -- Dairy-free
-(1007, 1013), -- Egg-free
-(1007, 1014), -- Soy-free
-(1008, 1004), -- Vegetarian
-(1008, 1012), -- Nut-free
-(1008, 1014); -- Soy-free
 
 
 INSERT INTO ingredient (ingredientID, ingredientName) VALUES 
@@ -70,6 +19,8 @@ INSERT INTO ingredient (ingredientID, ingredientName) VALUES
 (1004, 'Steak'),
 (1005, 'Ground beef'),
 (1006, 'Italian sausage'),
+(1007, 'Tri tip'),
+(1008, 'Pork shoulder'),
 (2001, 'Basil leaves'),
 (2002, 'Red bell pepper'),
 (2003, 'Green onions'),
@@ -96,12 +47,15 @@ INSERT INTO ingredient (ingredientID, ingredientName) VALUES
 (4006, 'Lasagna noodles'),
 (4007, 'Pasta'),
 (4008, 'All-purpose flour'),
+(4009, 'Macaroni'),
 (5001, 'Parmesan cheese'),
 (5002, 'Mozzarella cheese'),
 (5003, 'Shredded cheese'),
 (5004, 'Butter'),
 (5005, 'Ricotta cheese'),
 (5006, 'Unsalted butter'),
+(5007, 'Milk'),
+(5008, 'Cheddar cheese'),
 (6001, 'Pine nuts'),
 (6002, 'White beans'),
 (7001, 'Soy sauce'),
@@ -112,6 +66,8 @@ INSERT INTO ingredient (ingredientID, ingredientName) VALUES
 (7006, 'Tomato sauce'),
 (7007, 'Tomato paste'),
 (7008, 'Vegetable broth'),
+(7009, 'BBQ sauce'),
+(7010, 'Apple cider vinegar'),
 (8001, 'Salt'),
 (8002, 'Vegetable oil'),
 (8003, 'Black pepper'),
@@ -133,7 +89,11 @@ INSERT INTO ingredient (ingredientID, ingredientName) VALUES
 (8019, 'Onion powder'),
 (8020, 'Cinnamon'),
 (8021, 'Cornstarch'),
+(8022, 'Smoked paprika'),
+(8023, 'BBQ spice mix'),
 (9001, 'Furikake');
+
+
 
 
 INSERT INTO recipe_ingredient (recipeID, ingredientID) VALUES 
@@ -233,4 +193,77 @@ INSERT INTO recipe_ingredient (recipeID, ingredientID) VALUES
 (1008, 8001), -- Salt
 (1008, 8010), -- Sugar
 (1008, 8020), -- Cinnamon
-(1008, 8021); -- Cornstarch
+(1008, 8021), -- Cornstarch
+(1009, 1007), -- Tri tip
+(1009, 1008), -- Pork shoulder
+(1009, 2006), -- Broccoli
+(1009, 4009), -- Macaroni
+(1009, 5004), -- Butter
+(1009, 5007), -- Milk
+(1009, 5008), -- Cheddar cheese
+(1009, 7009), -- BBQ sauce
+(1009, 7010), -- Apple cider vinegar
+(1009, 8001), -- Salt
+(1009, 8003), -- Black pepper
+(1009, 8015), -- Garlic powder
+(1009, 8018), -- Olive oil
+(1009, 8022), -- Smoked paprika
+(1009, 8023); -- BBQ spice mix
+
+
+INSERT INTO restriction (restrictionID, restrictionName) VALUES 
+(1001, 'Low-carb'),
+(1002, 'Keto'),
+(1003, 'Vegan'),
+(1004, 'Vegetarian'),
+(1005, 'Pescatarian'),
+(1006, 'Low-fat'),
+(1007, 'Paleo'),
+(1008, 'Carnivore'),
+(1009, 'High-protein'), -- only with a (cal/10)/protein ratio <= 10/7 or 1.43
+(1010, 'Gluten-free'),
+(1011, 'Dairy-free'),
+(1012, 'Nut-free'),
+(1013, 'Egg-free'),
+(1014, 'Soy-free'),
+(1015, 'Grain-free'),
+(1016, 'Sugar-free'),
+(1017, 'Whole30'),
+(1018, 'Mediterranean'),
+(1019, 'Quick Meal'); -- 20 min or less
+
+
+INSERT INTO recipe_restriction (recipeID, restrictionID) VALUES 
+(1001, 1012), -- Nut-free
+(1002, 1006), -- Low-fat
+(1002, 1010), -- Gluten-free
+(1002, 1011), -- Dairy-free
+(1002, 1012), -- Nut-free
+(1003, 1010), -- Gluten-free
+(1003, 1011), -- Dairy-free
+(1003, 1012), -- Nut-free
+(1003, 1016), -- Sugar-free
+(1004, 1005), -- Pescatarian
+(1004, 1010), -- Gluten-free
+(1004, 1011), -- Dairy-free
+(1004, 1012), -- Nut-free
+(1004, 1013), -- Egg-free
+(1004, 1018), -- Mediterranean
+(1005, 1010), -- Gluten-free
+(1005, 1012), -- Nut-free
+(1005, 1013), -- Egg-free
+(1005, 1014), -- Soy-free
+(1005, 1015), -- Grain-free
+(1006, 1012), -- Nut-free
+(1006, 1014), -- Soy-free
+(1007, 1003), -- Vegan
+(1007, 1004), -- Vegetarian
+(1007, 1011), -- Dairy-free
+(1007, 1013), -- Egg-free
+(1007, 1014), -- Soy-free
+(1008, 1004), -- Vegetarian
+(1008, 1012), -- Nut-free
+(1008, 1014), -- Soy-free
+(1008, 1009), -- High-Protein
+(1009, 1012), -- Nut-free	
+(1009, 1014); -- Soy-free
